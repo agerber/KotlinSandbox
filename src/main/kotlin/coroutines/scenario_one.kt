9b8ebff0428838bde0b2b67f46fe5076399daccc
Adam Gerber <adam.gerber@yahoo.com>
2024-07-11 00:08:08 -0500

    more3b

--- src/main/kotlin/coroutines/scenario_one.kt
+++ src/main/kotlin/coroutines/scenario_one.kt
@@ -7,6 +7,8 @@ import kotlinx.coroutines.runBlocking
 fun scen01() = runBlocking {
     println("scen01")
     println("fetchDataFromServer-start(A)")  // edu.uchicago.gerber.code.main thread
+    println("UI is locked and completely unresponsive")
+    //simulate some long-running network operation by calling delay(2000)
     delay(2000)
     println("fetchDataFromServer-finish(Af)")
     println("showDataToUser(B)")
